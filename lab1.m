42ed5f134ddbf35829d6cc1494e32f546c14fd65
Adrian Edin <adred130 @student.liu.se>
2020-09-23 13:50:44 +0200

    Fixade format i given kod

--- lab1.m
+++ lab1.m
@@ -3,9 +3,9 @@ close all
 
 Ts = 0.1;
 Trange = [-15:Ts:15];
-s1 = exp(-0.1*Trange.ˆ2);
-s2 = exp(-0.1*Trange.ˆ2).*cos(Trange);
-E1 = sum(abs(s1.ˆ2));
-E2 = sum(abs(s2.ˆ2));
+s1 = exp(-0.1*Trange.^2);
+s2 = exp(-0.1*Trange.^2).*cos(Trange);
+E1 = sum(abs(s1.^2));
+E2 = sum(abs(s2.^2));
 s1 = s1/sqrt(E1);
 s2 = s2/sqrt(E2);
