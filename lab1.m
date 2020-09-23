e702acc329cfe16f624cd5ef481775decf4dc0bc
Adrian Edin <adred130 @student.liu.se>
2020-09-23 13:54:57 +0200

    Added plots of s1 and s2

--- lab1.m
+++ lab1.m
@@ -2,9 +2,16 @@ clear all
 close all
 
 Ts = 0.1;
-Trange = [-15:Ts:15];
-s1 = exp(-0.1*Trange.^2);
-s2 = exp(-0.1*Trange.^2).*cos(Trange);
+T_range = [-15:Ts:15];
+s1 = exp(-0.1*T_range.^2);
+s2 = exp(-0.1*T_range.^2).*cos(T_range);
+figure(1)
+plot (T_range,s1)
+hold on
+plot (T_range,s2)
+hold off
+legend("S1","S2")
+
 E1 = sum(abs(s1.^2));
 E2 = sum(abs(s2.^2));
 s1 = s1/sqrt(E1);
