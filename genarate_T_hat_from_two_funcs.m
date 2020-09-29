f7b0be6ca551c938a81140e4cffc462d6c1fb311
Adrian Edin <adred130 @student.liu.se>
2020-09-29 17:21:09 +0200

    Working monte carlo sim.

---
+++ genarate_T_hat_from_two_funcs.m
@@ -0,0 +1,18 @@
+function hat = genarate_T_hat_from_two_funcs(s,x)
+    % conv får ut 601 index. Vilket blir -30 till 30 sekunder
+    % varje sekund motsvarar 10 index. NOllan gör det sista samplet, därför
+    % ska nollan ingå i en delmängd över origo
+    T_hat_metric = conv(s,x);
+    % plot(T_hat_metric)
+    % För att få de mittesta -5 till 5 sekunderna plockar jag 101 index
+    % från mitten. Jag ska ha ~1/6 av alla index. 
+    % 25 sekunder in är index 250 (men på matlabianska är det "index" 251)
+    % och sen 101 index från det blir 351. på
+    % andra sidan är det då 250 index kvar, ok.
+    start_index = 251;
+    end_index = 351;
+    T_hat_metric = T_hat_metric(start_index:end_index);
+    [max_value, max_t_index] = max(T_hat_metric);
+    
+    hat = -5+ 0.1*(max_t_index-1);
+end
--- lab1.m
+++ lab1.m
@@ -1,7 +1,6 @@
 clear all
 close all
 
-
 T_s = 0.1;
 T_range = [-15:T_s:15];
 s1 = exp(-0.1*T_range.^2);
@@ -12,19 +11,24 @@ E1 = sum(abs(s1.^2));
 E2 = sum(abs(s2.^2));
 
 % Normalized signals => signal energy =1
-s1 = s1/sqrt(E1);
-s2 = s2/sqrt(E2);
+alpha_1 = 1/sqrt(E1);
+alpha_2 = 1/sqrt(E2);
+s1 = s1*alpha_1;
+s2 = s2*alpha_2;
 
 % SNR
-SNR = 30;
+SNR = 5;
 sigma2 = 10^(-SNR/10);
 
 % Random time diff calc
-T = -5 + 10*rand(1);
+T = 2% -5 + 10*rand(1);
+
+s1_time_diffed = exp(-0.1*(T_range-T).^2);
+s2_time_diffed = exp(-0.1*(T_range-T).^2).*cos((T_range-T));
 
 %% a)
-% s2 is better because of greater derivate which is looked for in CRB
-% See (3.14) in kay I
+% s2 is better because of greater derivate which is looked for in CRB See
+% (3.14) in kay I
 figure(1)
 plot (T_range,s1,'r')
 hold on
@@ -32,31 +36,70 @@ plot (T_range,s2,'b')
 legend("S1","S2")
 
 % Lägger till brus
-x1 = exp(-0.1*(T_range-T).^2) + sqrt(sigma2)*randn(1,length(T_range));
+w = exp(-0.1*(T_range-T).^2) + sqrt(sigma2)*randn(1,length(T_range));
 x2 = exp(-0.1*(T_range-T).^2).*cos(T_range-T) + sqrt(sigma2)*randn(1,length(T_range));
 
-x1 = x1/sqrt(E1);
+w = w/sqrt(E1);
 x2 = x2/sqrt(E2);
 figure(1)
-plot(T_range,x1,'m')
+plot(T_range,w,'m')
 plot(T_range,x2,'c')
 hold off
-%%
 
 T_search = -5:T_s:5;
 T_hat_metric = zeros(1,length(T_search));
 
+% ML estimatorn för s1
 for i = 1:1:length(T_search)
     t = T_search(i);
     intermidiate = 1/sqrt(E1)*exp(-0.1*(T_range-t).^2);
-    T_hat_metric(i) = sum(x1.*intermidiate);
+    T_hat_metric(i) = sum(w.*intermidiate);
 end
 
+[max, max_t_index] = max(T_hat_metric);
+genarate_T_hat_from_two_funcs(s1,w);
+T_search(max_t_index)
 figure(3)
 plot(T_search,T_hat_metric,'r')
-[max, max_t_index] = max(T_hat_metric);
-T_search(max_t_index)
+%% Monte Carlo
+% testa antalat körningar
+rng('shuffle')
 
+M0 = 1:100:5001; % All the different number of monte-carlo runs we will try
+N = length(T_range);
+A_hat_mean = zeros(2,M0(end));
+A_hat_std = zeros(2,M0(end));
 
+for M=M0
+    
+    A_hat_s1 = zeros(1,M );
+    A_hat_s2 = zeros(1,M );
+    for m=1:M
+        % s1 and s2 contains the signal without noise.
+        w =  sqrt(sigma2)*rand(1,N);
+        A_hat_s1(m) = genarate_T_hat_from_two_funcs(s1,w+s1_time_diffed);
+        A_hat_s2(m) = genarate_T_hat_from_two_funcs(s2,w+s2_time_diffed);
+    end
+    A_hat_mean(1,M) = mean(A_hat_s1);
+    A_hat_std(1,M) = std(A_hat_s1);
+    A_hat_mean(2,M) = mean(A_hat_s2);
+    A_hat_std(2,M) = std(A_hat_s2);
+    
+end
+
+mean_plot_1 = A_hat_mean(1,:);
+std_plot_1 = A_hat_std(1,:);
+mean_plot_2 = A_hat_mean(2,:);
+std_plot_2 = A_hat_std(2,:);
+
+figure(25)
+plot(M0,mean_plot_1(M0),M0,mean_plot_2(M0))
+legend("s1","s2")
+title("mean")
+
+figure(26)
+plot(M0,std_plot_1(M0),M0,std_plot_2(M0))
+legend("s1","s2")
+title("std")
 
 
