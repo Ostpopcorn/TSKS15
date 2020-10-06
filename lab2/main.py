622b983a5739059f2ee0e7efca13d2e376dc1cae
Adrian Edin <adred130@student.liu.se>
2020-10-06 17:35:58 +0200

    En super duper mega funktion som löser tonklassificeringen för ett godtyckligt antal olika pitchar.

--- lab2/main.py
+++ lab2/main.py
@@ -25,30 +25,38 @@ a = rnd.standard_exponential(1)
 SNR = 10
 k = len(tone)  # length of one note
 n = np.arange(k)  # array [0, ... ,k-1]
+pitch_offsets = [0.975, 1.025]
 
 # %%
 """
 Generate H_i for all twelve notes.
 All f_i exists in sg.dict_note2frequency
 """
-# Contains {"C",H-matix}
+# Contains { 0.975 = {"C" = H-matix, ....}
+#            1.025 = {"C" = H-matix, ....} }
 h_matrices_single_note = {}
-
-for note_name, note_freq in sg.dict_note2frequency.items():
-    h_matrices_single_note[note_name] = np.zeros((k, 2))
-    h_matrices_single_note[note_name][:, 0] = np.cos(2 * np.pi * note_freq * n / fs)
-    h_matrices_single_note[note_name][:, 1] = np.sin(2 * np.pi * note_freq * n / fs)
-
 h_matrices_triple_note = {}
 
-for note_name, note_freq in sg.dict_note2frequency.items():
-    h_matrices_triple_note[note_name] = np.zeros((k, 6))
-    h_matrices_triple_note[note_name][:, 0] = np.cos(2 * np.pi * note_freq * 1 * n / fs + phi)
-    h_matrices_triple_note[note_name][:, 1] = np.sin(2 * np.pi * note_freq * 1 * n / fs + phi)
-    h_matrices_triple_note[note_name][:, 2] = np.cos(2 * np.pi * note_freq * 3 * n / fs + phi)
-    h_matrices_triple_note[note_name][:, 3] = np.sin(2 * np.pi * note_freq * 3 * n / fs + phi)
-    h_matrices_triple_note[note_name][:, 4] = np.cos(2 * np.pi * note_freq * 5 * n / fs + phi)
-    h_matrices_triple_note[note_name][:, 5] = np.sin(2 * np.pi * note_freq * 5 * n / fs + phi)
+for pitch_offset in pitch_offsets:
+    h_matrices_single_note[pitch_offset] = {}
+    current_h_matrices_single_note = h_matrices_single_note[pitch_offset]
+
+    for note_name, note_freq in sg.dict_note2frequency.items():
+        current_h_matrices_single_note[note_name] = np.zeros((k, 2))
+        current_h_matrices_single_note[note_name][:, 0] = np.cos(2 * np.pi * note_freq * pitch_offset * n / fs)
+        current_h_matrices_single_note[note_name][:, 1] = np.sin(2 * np.pi * note_freq * pitch_offset * n / fs)
+
+    h_matrices_triple_note[pitch_offset] = {}
+    current_h_matrices_triple_note = h_matrices_triple_note[pitch_offset]
+
+    for note_name, note_freq in sg.dict_note2frequency.items():
+        current_h_matrices_triple_note[note_name] = np.zeros((k, 6))
+        current_h_matrices_triple_note[note_name][:, 0] = np.cos(2 * np.pi * note_freq * pitch_offset * 1 * n / fs)
+        current_h_matrices_triple_note[note_name][:, 1] = np.sin(2 * np.pi * note_freq * pitch_offset * 1 * n / fs)
+        current_h_matrices_triple_note[note_name][:, 2] = np.cos(2 * np.pi * note_freq * pitch_offset * 3 * n / fs)
+        current_h_matrices_triple_note[note_name][:, 3] = np.sin(2 * np.pi * note_freq * pitch_offset * 3 * n / fs)
+        current_h_matrices_triple_note[note_name][:, 4] = np.cos(2 * np.pi * note_freq * pitch_offset * 5 * n / fs)
+        current_h_matrices_triple_note[note_name][:, 5] = np.sin(2 * np.pi * note_freq * pitch_offset * 5 * n / fs)
 
 # %%
 """ 
@@ -57,23 +65,30 @@ Task one, single tone generator
 print("start part one")
 
 
-def note_detector(note):
-    h_value = {}
-    for current_note_name, h_matrix in h_matrices_single_note.items():  # h_matrix) **2
-        h_value[current_note_name] = np.linalg.norm(np.matmul(np.transpose(tone), h_matrix)) ** 2
-    max_note = max(h_value, key=h_value.get)
-    return max_note, h_value
+def note_detector(note, h_matrices):
+    h_values = {}
+    max_notes_per_pitch = {}
+    for current_pitch_offset in pitch_offsets:
+        h_values[current_pitch_offset] = {}
+        current_h_values = h_values[current_pitch_offset]
+        for current_note_name, h_matrix in h_matrices[current_pitch_offset].items():  # h_matrix) **2
+            current_h_values[current_note_name] = np.linalg.norm(np.matmul(np.transpose(tone), h_matrix)) ** 2
+        current_max_note = max(current_h_values, key=current_h_values.get)
+        max_notes_per_pitch[current_pitch_offset] = current_max_note
+    pitch = max(max_notes_per_pitch, key=lambda val: h_values[val][max_notes_per_pitch[val]])
+    print("Pitch", pitch, "Note", max_notes_per_pitch[pitch])
+    max_note = max_notes_per_pitch[pitch]
+    return max_note, pitch, h_values
 
 
 # Contains {"C",||y_n^T*H_j||^2}
-max_note, h_values = (note_detector(tone))
-print("Tone is: ", max_note, "with power", h_values[max_note])
+max_note, pitch, returned_h_values = note_detector(tone, h_matrices_single_note)
+print("Tone is: ", max_note, "with power", returned_h_values[pitch][max_note])
 plt.figure()
-plt.plot(np.arange(len(h_values)), h_values.values())
+plt.plot(np.arange(len(returned_h_values[pitch])), returned_h_values[pitch].values())
 plt.xlabel('H')
 plt.ylabel('magnitude')
 plt.savefig('tone_many_j.png')
 print("end task one")
 
 # %%
-
